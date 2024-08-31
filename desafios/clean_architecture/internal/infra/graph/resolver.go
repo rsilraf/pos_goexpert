75f1cb9560c1a4407665766e92a5557004027f61
Rafael da Silva <rsilraf@gmail.com>
2024-08-31 18:30:08 +0100

    Adiciona código do desafio clean architecture.

---
+++ desafios/clean_architecture/internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/rsilraf/pos_goexpert/desafios/clean_architecture/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
